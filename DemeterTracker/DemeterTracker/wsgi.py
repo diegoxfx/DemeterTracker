2d4259c2574f5bde4c6b1299f00dffc43cd3a587
Diego Alejandro Cifuentes García <diegoxfx@turing-machine>
2019-01-25 16:20:37 -0500

    autenticacion

---
+++ DemeterTracker/DemeterTracker/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for DemeterTracker project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.1/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'DemeterTracker.settings')
+
+application = get_wsgi_application()
